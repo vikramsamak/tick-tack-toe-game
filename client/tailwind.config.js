cce94024b68882bbc911caf25089b1ac86a9727f
vikramsamak <vikramsamak02@gmail.com>
2024-11-24 11:08:47 +0530

    default theme :  dark

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark", "light"], // Ensure both themes are available
+  },
 };
